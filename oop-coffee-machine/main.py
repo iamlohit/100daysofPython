e97ebfefb5b4ea459203ec93e90334c8700adbda
Lohit <jethwanilohit@gmail.com>
2022-05-25 11:52:33 +0530

    Coffee maker program, this time using Classes & Objects OOP.

---
+++ oop-coffee-machine/main.py
@@ -0,0 +1,22 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu_item = MenuItem
+
+status = True
+while status:
+    order = input(f"What would you like ? {menu.get_items()}:  ")
+    if order == "off":
+        status = False
+        break
+    elif order == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
