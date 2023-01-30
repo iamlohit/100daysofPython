d076a7ad25d66fd700f287f3f7bfa7a61480686c
Lohit <jethwanilohit@gmail.com>
2023-01-30 11:05:39 +0530

    Updated logic

--- rock-paper-scissors/main.py
+++ rock-paper-scissors/main.py
@@ -1,61 +1,61 @@
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
+replay = True
+while replay == True:
+  rock = '''
+      _______
+  ---'   ____)
+        (_____)
+        (_____)
+        (____)
+  ---.__(___)
+  '''
+  
+  paper = '''
+      _______
+  ---'   ____)____
+            ______)
+            _______)
+           _______)
+  ---.__________)
+  '''
+  
+  scissors = '''
+      _______
+  ---'   ____)____
+            ______)
+         __________)
+        (____)
+  ---.__(___)
+  '''
+  
+  #Write your code below this line 👇
+  import random
+  dice = [rock, paper, scissors]
+  user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+  computer_choice = random.randint(0, 2)
+  print(dice[user_choice])
+  print(f"Computer chose:{dice[computer_choice]}")
+  # if user_choice == computer_choice:
+  #   print("It's a draw")
+  # elif user_choice == 0 and computer_choice == 1:
+  #   print ("You lose")
+  # elif user_choice == 0 and computer_choice == 2:
+  #   print("You Win!")
+  # elif user_choice == 1 and computer_choice == 0:
+  #   print("You Win!")
+  # elif user_choice == 1 and computer_choice == 2:
+  #   print("You loose.")
+  # elif user_choice == 2 and computer_choice == 0:
+  #   print("You loose.")
+  # else:
+  #   print("You Win!")
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
-
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
-
-#Write your code below this line 👇
-import random
-
-game = [rock, paper, scissors]
-
-p_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
-
-if p_choice >= 3 or p_choice < 0:
-	print("Please enter a valid input")
-else: 
-	print(game[p_choice])
-	
-	c_choice = random.randint(0, 2)
-	
-	print("Computer chose:")
-	
-	print(game[c_choice])
-	
-	#0 ROCK
-	#1 PAPER
-	#2 SCISSOR
-	
-	
-	if p_choice == 0 and c_choice == 2:
-		print("You win!")
-	elif c_choice == 0 and p_choice == 2:
-		print("You loose")
-	elif c_choice > p_choice:
-		print("You lose!")
-	elif p_choice > c_choice:
-		print("You win!")
-	elif p_choice == c_choice:
-		print("Its a draw!")
-	
+  if user_choice == 0 and computer_choice == 2:
+    print("You Win!")
+  elif user_choice > computer_choice:
+    print("You Win!")
+  elif user_choice < computer_choice:
+    print("You Loose.")
+  elif user_choice == computer_choice:
+    print("It's a draw.")
+  else:
+    print("You entered and invalid number, you LOosee!")
