4156a47c513ea87f858d728a23d74db08c3b4e1a
Lohit <jethwanilohit@gmail.com>
2023-06-09 11:07:31 +0530

    Reorganized Files

---
+++ folder_creation.py
@@ -0,0 +1,7 @@
+import os
+
+list = ['Day 12','Day 13','Day 14',
+		'Day 15','Day 16','Day 17','Day 18','Day 19','Day 20']
+
+for items in list:
+	os.mkdir(items)
---
+++ gauss_calc.py
@@ -0,0 +1,5 @@
+total = [0]
+for number in range(0, 101, 2):
+    total.append(number) 
+
+print(sum(total))
