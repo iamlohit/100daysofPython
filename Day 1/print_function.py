7fa19c95c0fbe680e3d446870cdb24e91576ae4f
Lohit <jethwanilohit@gmail.com>
2023-06-11 18:56:43 +0530

    Day 1 in progress - pausing for the day

---
+++ Day 1/print_function.py
@@ -0,0 +1,18 @@
+print("Hello World")
+
+# Way 1 - Single Line Print
+print("Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# Way 2 - Multi Line Print
+print('''Python Print Function
+The function is declared like this:
+print("print('what to print')''')
+
+# Way 3 - Single Line with Delimiter
+print("Python Print Function\nThe function is declared like this:\nprint('what to print')")
+
+# Strings are always printed in ""
+# Numbers donot need "" as seen below
+print(1)
---
+++ Day 1/string_manupulation.py
@@ -0,0 +1,22 @@
+# String Concatenation
+print("Hello" + "World")
+
+# Spaces are not automatically added
+print("Hello" + " " + "World")
+
+# Another Way to do the same
+print("Hello " + "World")
+
+#Indentation is key in Python
+ print("Hello " + "World")
+
+# Note: You see we added a space or indent
+# on the 3rd print statement
+# while the first 3 statements are correct
+# But we get an error when we try to run the
+# file now.
+# This is because Python is a COMPILED Language
+# & NOT an INTERPRETTED Language
+# Meaning the code is fully checked in it's
+# Entirity before being run, and is NOT RUN
+# line by line.
