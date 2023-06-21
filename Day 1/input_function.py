d575a7da0389ffe0dfc47b8d665d8650049cc305
Lohit <jethwanilohit@gmail.com>
2023-06-21 17:44:19 +0530

    Understanding the Input function

---
+++ Day 1/input_function.py
@@ -0,0 +1,16 @@
+# Write a program ask's your name &
+# prints the number of characters in a user's name.
+
+# username = input("What is your name? ")
+# print(len(username))
+
+# Another way to do the above in a single line:
+
+print(len(input("What is your name? ")))
+
+# Write a program that ask's your name and greets you, in a single line.
+
+# print("Hello " + input("What is your name? "))
+
+# The reason this works is that,
+# The inner most () is executed first and then outwards!
